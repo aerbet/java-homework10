7c7b3f939e2730e03edde822c1d1bed4d80c0df3
Erbol Zhaparov <ae.270718@gmail.com>
2025-07-27 21:03:07 +0600

    finished task 2 & 3

--- Task2.java
+++ Task2.java
@@ -1,6 +1,35 @@
 public class Task2 {
     
     public static void main(String[] args) {
+        int x = 1;
+        double equation1 = 2 * x; // a
+
+        double equation2 = Math.sin(x); // б
+
+        int a = 2;
+        double equation3 = Math.pow(a, 2); // в
+
+        double equation4 = Math.sqrt(x); // г
+
+        int n = 1;
+        double equation5 = Math.abs(n); // д
         
+        int y = 1;
+        double equation6 = 5 * Math.cos(y); // е
+
+        double equation7 = -7.5 * Math.pow(a, 2); // ж
+
+        double equation8 = 3 * Math.sqrt(x); // з
+
+        float alpha = 1;
+        float beta = 1;
+        double equation9 = Math.sin(alpha) * Math.cos(beta) + Math.cos(alpha) * Math.sin(beta); // и
+        
+        int b = 1;
+        double equation10 = a * Math.sqrt(2 * b); // к
+
+        double equation11 = 3 * Math.sin(2 * alpha) * Math.cos(3 * beta); // л
+
+        double equation12 = -5 * Math.sqrt(x + Math.sqrt(y)); // м
     }
 }
--- Task3.java
+++ Task3.java
@@ -1,6 +1,30 @@
 public class Task3 {
     
     public static void main(String[] args) {
+        double x = 1;
+        double equation1 = -1.0 / Math.pow(x, 2); // a
+
+        double a = 5;
+        double b = 2;
+        double c = 2;
+        double equation2 = a / (b * c); // б
+
+        double equation3 = (a / b) * c; // в
         
+        double equation4 = (a + b) / 2; // г
+
+        double equation5 = 5.45 * (a + 2 * b) / (2 - a); // д
+        
+        double equation6 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a); // е
+        
+        double equation7 = (-b + 1.0 / a) / (2.0 / c); // ж
+        
+        double equation8 = 1.0 / (1 + (a + b) / 2.0); // з
+        
+        double equation9 = 1.0 / (1 + 1.0 / (2 + 1.0 / (2 + 3.0 / 5.0))); // и
+        
+        double m = 1;
+        double n = 1;
+        double equation10 = Math.pow(2, Math.pow(m, n)); // к
     }
 }
