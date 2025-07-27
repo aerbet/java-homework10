d0c80c052ad3e4b997b0a1f78a21b22b2bfd5e93
Erbol Zhaparov <ae.270718@gmail.com>
2025-07-27 17:06:25 +0600

    finished task1

---
+++ Task4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Task4 {
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double tariff = 0.77;
+
+        System.out.print("Введите предыдущее показание электросчетчика: ");
+        int pvCount = sc.nextInt();
+
+        System.out.print("Введите следующее показание электросчетчика: ");
+        int nxCount = sc.nextInt();
+
+        int amountOfEnergy = Math.abs(nxCount - pvCount);
+
+        double result = amountOfEnergy * tariff;
+
+
+        System.out.println("Вы потребили: " + amountOfEnergy + " кВт");
+        System.out.println("Тариф: " + tariff + " сом");
+        System.out.println("К оплате: " + result + " сом");
+
+        sc.close();
+    }
+}
